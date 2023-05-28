a7c060a09b7cd17c930eaf3f7e3d96f7641eaeae
Abud-B <82672215+Abud-B@users.noreply.github.com>
2023-05-28 21:43:51 +0100

    Update Uno_OOP.py
    fixed some errors. made improvements with deck creation, handling +4 and wild cards, handling reverse cards if there are two players and giving a penalty if a player does not say UNO.

--- Uno_OOP.py
+++ Uno_OOP.py
@@ -24,15 +24,21 @@ class Card:
 class Deck:
     def __init__(self):
         self._cards = []
-        self._valid_colors = ['Red', 'Yellow', 'Green', 'Blue']
 
     def create_deck(self):
-        colors = ['Red', 'Yellow', 'Green', 'Blue']
-        values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Reverse', 'Draw Two', 'Wild', '+4']
+            colors = ['Red', 'Yellow', 'Green', 'Blue']
+            values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Reverse', 'Draw Two']
+            special_values = ['Wild', '+4']
 
-        for color in colors:
-            for value in values:
-                self._cards.append(Card(color, value))
+            for color in colors:
+                for value in values:
+                    for _ in range(2):
+                        self._cards.append(Card(color, value))
+                self._cards.append(Card(color, '0'))  # Only one '0' card per color
+
+            for value in special_values:
+                for _ in range(4):  # Four 'Wild' and '+4' cards each
+                    self._cards.append(Card(None, value))  # No color assigned initially
 
     def shuffle(self):
         random.shuffle(self._cards)
@@ -40,17 +46,11 @@ class Deck:
     def draw_card(self):
         return self._cards.pop()
 
-    def get_valid_colors(self):
-        return self._valid_colors
-
-    def set_valid_colors(self, valid_colors):
-        self._valid_colors = valid_colors
-
-
 class Player:
     def __init__(self, name):
         self._name = name
         self._hand = []
+        self._said_uno = False
 
     @property
     def name(self):
@@ -65,66 +65,75 @@ class Player:
     
     def get_hand(self):
         return self._hand
+    
+    def add_card(self, card):
+        self._hand.append(card)
 
     def is_valid_move(self, card, top_card, game):
-        return (
-            card.get_color() == top_card.get_color()
-            or card.get_value() == top_card.get_value()
-            or card.get_value() in ["Wild", "+4"]
-        )
+            if card.get_value() in ["Wild", "+4"]:
+                if card.get_value() == "+4":
+                    # Check if player has no other cards of the current color in their hand
+                    for c in self._hand:
+                        if c.get_color() == top_card.get_color():
+                            return False
+                return True  # 'Wild' and '+4' cards can be played at any time
+
+            return (
+                card.get_color() == top_card.get_color()
+                or card.get_value() == top_card.get_value()
+            )
 
     def play_card(self, card, discard_pile, game):
         if card in self._hand:
             top_card = discard_pile[-1]
 
             if self.is_valid_move(card, top_card, game):
+                # if the player is about to have one card left, ask them to say 'UNO'
+                if len(self._hand) == 2:
+                    say_uno = input("You're about to have only one card left! Say 'UNO' (Y/N): ")
+                    self._said_uno = say_uno.upper() == 'Y'
+
+                if not self._said_uno and len(self._hand) == 2:
+                    print(f"{self._name} failed to say 'UNO'! Draw 2 cards as a penalty.")
+                    self.draw_card(game.get_deck(), 2)
+
                 self._hand.remove(card)
                 discard_pile.append(card)
-                
-                if len(self._hand) == 1:
-                    say_uno = input("Say 'UNO' if you have only one card left (Y/N): ")
-                    while say_uno.upper() not in ["Y", "N", "UNO"]:
-                        print("Invalid input! Please enter 'Y' or 'N'.")
-                        say_uno = input("Say 'UNO' if you have only one card left (Y/N): ")
-
-                    if say_uno.upper() != "Y":
-                        print("You failed to say 'UNO'! Draw 2 cards as a penalty.")
-                        self.draw_card(game.get_deck(), 2)
 
                 if card.get_value() == "Wild":
-                    valid_colors = game.get_deck().get_valid_colors()
-                    color = self.select_color(valid_colors)
+                    color = self.select_color()
                     card.set_color(color)
-                    valid_colors.append(color)
 
                 elif card.get_value() == "+4":
-                    valid_colors = game.get_deck().get_valid_colors()
-                    color = self.select_color(valid_colors)
+                    color = self.select_color()
                     card.set_color(color)
-                    valid_colors.append(color)
                     next_player = game.get_next_player()
                     next_player.draw_card(game.get_deck(), 4)
                     game.skip_turn()
 
-                if card.get_color() != "Wild" and card.get_color() in game.get_deck().get_valid_colors():
-                    game.get_deck().get_valid_colors().remove(card.get_color())
-
                 if card.get_value() == "Skip":
                     game.skip_turn()
                 elif card.get_value() == "Reverse":
-                    game.reverse_direction()
+                     # if there are only two players treat reverse as a skip
+                    if len(game._players) == 2: 
+                        game.skip_turn()  
+                    else:
+                        game.reverse_direction() 
                 elif card.get_value() == "Draw Two":
                     next_player = game.get_next_player()
                     next_player.draw_card(game.get_deck(), 2)
-                    game.skip_turn()  # Skip the next player's turn when a "Draw Two" card is played
+                    # skip the next player's turn after they draw the cards
+                    game.skip_turn()  
             else:
-                print("Invalid move! The selected card cannot be played.")
+                print("Invalid move! The selected card cannot be played.\nDraw 2 cards for mistake")
+
         else:
             print("Invalid choice! Please enter a valid index.")
 
 
-    def select_color(self, valid_colors):
-        print("Select a valid color:")
+    def select_color(self):
+        valid_colors = ['Red', 'Yellow', 'Green', 'Blue']
+        print("Select a color:")
         for i, color in enumerate(valid_colors):
             print(f"{i + 1}. {color}")
 
@@ -145,7 +154,7 @@ class Game:
         self._deck = Deck()
         self._discard_pile = []
         self._direction = 1
-        self._current_player_index = 0
+        self._current_player_index = 1
 
     def start_game(self):
         self._deck.create_deck()
@@ -156,8 +165,9 @@ class Game:
         special_values = ['Skip', 'Reverse', 'Draw Two', 'Wild', '+4']
         
         while top_card.get_value() in special_values:
-            self._deck._cards.insert(0, top_card)  # Put the special card back into the deck
-            top_card = self._deck.draw_card()  # Draw another card
+            # if the top card is a special card, put it back and get another card
+            self._deck._cards.insert(0, top_card)  
+            top_card = self._deck.draw_card()  
 
         self._discard_pile.append(top_card)
         self.print_game_status()
@@ -186,7 +196,7 @@ class Game:
         return self._players[self._current_player_index]
 
     def get_next_player_index(self):
-        return (self._current_player_index + self._direction) % len(self._players)
+        return (self._current_player_index + self._direction + len(self._players)) % len(self._players)
 
     def skip_turn(self):
         self._current_player_index = self.get_next_player_index()
@@ -204,8 +214,12 @@ class Game:
 
         valid_cards = self.get_valid_moves()
         print("Valid cards:")
-        for i, card in enumerate(valid_cards):
-            print(f"{i + 1}. {card}")
+        if valid_cards:  
+            for i, card in enumerate(valid_cards):
+                print(f"{i + 1}. {card}")
+        else:  
+            # tell the player to draw a card if there are no valid moves
+            print("No valid cards in your hand. You will have to draw a card.")
 
         card_choice = self.get_card_choice(valid_cards)
 
@@ -213,9 +227,9 @@ class Game:
             current_player.play_card(card_choice, self._discard_pile, self)
         else:
             drawn_card = self._deck.draw_card()
-            current_player.hand.append(drawn_card)
+            current_player.add_card(drawn_card)
             print(f"You drew a {drawn_card}.")
-            
+                    
             # check if the drawn card is valid
             top_card = self._discard_pile[-1]
             if current_player.is_valid_move(drawn_card, top_card, self):
@@ -255,6 +269,8 @@ class Game:
 
     def get_next_player(self):
         return self._players[self.get_next_player_index()]
+
+
 
 def main():
     num_players = int(input("Enter the number of players: "))
